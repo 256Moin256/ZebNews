b9ab3d550e793f2d1374beedcf0983239d2f9040
Moin <pathanmoinpathan124@gmail.com>
2023-03-27 07:15:14 +0530

    Version 1.0

---
+++ controller/App.controller.js
@@ -0,0 +1,22 @@
+sap.ui.define(
+    [
+    'sap/ui/core/mvc/Controller'
+    ], 
+    function(Controller) {
+    'use strict';
+    return Controller.extend("zebnews.controller.App", {
+
+        onInit: function() {
+           
+        },
+        onExit: function() {
+            
+        },
+        onBeforeRendering: function() {
+            
+        },
+        onAfterRendering: function() {
+            
+        }
+    });
+});
